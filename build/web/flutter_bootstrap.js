73be84086aa1c3a729562b72a6a4dda011634ed1
Eduardo Carvalho de Oliveira <eduardo.carvalho@conlogsa.com.br>
2025-12-13 23:07:19 -0300

    Rebuild Flutter Web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4035566357"
+    serviceWorkerVersion: "1528620341"
   }
 });
